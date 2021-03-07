5b999f503b15c8fa249a602f4e63037c7154aad2
dario <dario.lacuesta@hotmail.com>
2021-03-07 19:43:00 -0300

    Commit 07/03

---
+++ src/components/CardWidget.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Button.css'
+
+const STYLES = [
+    'btn--primary',
+    'btn--outline'
+]
+
+const SIZES = [
+    'btn--medium',
+    'btn--large'
+]
+
+export const CardWidget = ({
+    children,
+    type,
+    onClick,
+    cartStyle,
+    cartSize
+})=>{
+    const checkCartStyle = STYLES.includes(cartStyle) ? cartStyle : STYLES[0]
+    const checkCartSize = SIZES.includes(cartSize) ? cartSize : SIZES[0]
+
+    return (
+      <button className={`btn ${checkCartStyle} ${checkCartSize}`} onClick={onClick}type={type}>
+            {children}
+        </button>)}
+export default CardWidget;
---
+++ src/components/Navbar/NavBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {MenuItems} from "./MenuItems"
+import {Button} from '../Button'
+import './Navbar.css'
+import CardWidget from '../CardWidget'
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+
+
+class Navbar extends React.Component {
+    state = {clicked:false}
+
+    handleClick = ()=>{
+        this.setState({clicked:!this.state.clicked})
+    }
+
+    render(){
+        return(
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">Music Store <i className="fas fa-compact-disc"></i></h1>
+                <div className="menu-icon" onClick={this.handleClick}>
+                   <i className={this.state.clicked? 'fas fa-times':'fas fa-bars'}></i>
+                </div>
+                <ul className={this.state.clicked? 'nav-menu active' : 'nav-menu'}>
+                    {MenuItems.map((item,index)=>{
+                    return(<li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                {item.title}
+                                </a>
+                            </li>)
+                    })}
+                </ul>
+                <Button>Sign Up</Button>
+               <CardWidget><AddShoppingCartIcon style={{ fontSize:30 }}></AddShoppingCartIcon></CardWidget>
+            </nav>
+        )
+    }
+}
+export default Navbar
