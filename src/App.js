f8f4bdf64bdafbb3c124872260ac4952d3cb84ec
dario <dario.lacuesta@hotmail.com>
2021-03-09 20:25:15 -0300

    commit 3ra entrega

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from "./components/Navbar/NavBar"
 import './App.css';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
  <div className="App">
    <Navbar/>
+   <ItemListContainer curso="React" proyecto ="proyecto"/>
  </div>
   );}
 
