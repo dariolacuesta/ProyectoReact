a612cf78049d635a1c5a3d3d616e97b38a342ffa
dario <dario.lacuesta@hotmail.com>
2021-03-16 00:12:30 -0300

    16/03

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,18 +1,26 @@
-import { useState } from "react";
+import { useState,useRef } from "react";
 
 const ItemCount = ({stock,initial,onAdd}) => {
+const [counter,setCurrentStock] = useState(0);
+const onChange = (e)=>{
+e.preventDefault();
+const  actualStock = stock - e.target.value
+if(actualStock<0)
+    counterInput.current.disabled = true
+else
+    counterInput.current.disabled = false
 
+    setCurrentStock(e.target.value);
+};
+const counterInput = useRef(null)
 
 return <form className="itemcount">
      <p className="stock">Stock: {stock}</p>
     <br/>
-    <input id='cantidad' type="number" placeholder={initial}></input>
+    <input onChange={(e)=>onChange(e)} id='cantidad' type="number" placeholder={initial} min="0" step="1"></input>
     <br/>
-    <button onClick ={(e)=> onAdd(e,1)}  >Agregar al Carrito</button>
+    <button onClick ={(e)=> onAdd(e,counter)} ref={counterInput} >Agregar al Carrito</button>
 </form>
 };
-
-
-
 
 export default ItemCount;
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -7,16 +7,13 @@ const ItemListContainer = ({curso,proyecto})=>{
     const [currentStock,setCurrentStock] = useState(5)
     const restartStock = (e,newStock)=>{
     e.preventDefault();
-    // if(currentStock === 0){
-    //     buttonState = 'true'
-    // }
-    setCurrentStock((currentStock)=>currentStock-newStock);}
+
+    setCurrentStock((currentStock)=>(currentStock-newStock));}
 
 return(
 <div className="itemListContainer fa-3x" >
     <h2>Bienvenido a mi {proyecto}</h2>
     <p> Este sera el area utilizado para renderizar elementos en el {proyecto} final de {curso}</p>
-    <p>Sitio aun en Construcción</p>
     <ItemCount stock={currentStock} initial={1} onAdd={restartStock}/>
  </div>
  )};
